ea61bbcd6062423b89a68b205c76ef26480256b3
Gang Yi <khorgangyi@gmail.com>
2024-04-21 13:14:01 +0100

    return error for checking

--- src/pages/api/isSetup/index.ts
+++ src/pages/api/isSetup/index.ts
@@ -10,7 +10,7 @@ async function publishImageHandler(req: NextApiRequest, res: NextApiResponse) {
 			if (process.env.NODE_ENV === "development") {
 				console.error(err);
 			}
-			res.status(500).end();
+			res.status(500).json({ err });
 		}
 	} else {
 		res.status(400).end();
