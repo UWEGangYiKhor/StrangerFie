94769ea7d3f80d7f801932b50c60952973415d7c
Gang Yi <khorgangyi@gmail.com>
2024-04-21 22:30:07 +0100

    Revert "Server check"
    This reverts commit 2d7564779170481699ae60cf26d8cd31c2e16d91.

--- src/pages/api/isSetup/isSetupServices.ts
+++ src/pages/api/isSetup/isSetupServices.ts
@@ -2,12 +2,11 @@ import { isSetupResponses } from "@/responses/isSetupResponses";
 import prisma from "@/utils/prismaClient";
 
 export default async function isSetupServices(): Promise<isSetupResponses> {
-	// const haventPublishCount = await prisma.publish_image.count({
-	// 	where: {
-	// 		archived: false,
-	// 	},
-	// });
+	const haventPublishCount = await prisma.publish_image.count({
+		where: {
+			archived: false,
+		},
+	});
 
-	// return { status: haventPublishCount > 0 };
-	return { status: true };
+	return { status: haventPublishCount > 0 };
 }
