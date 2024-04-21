cec60e114f5f7b5bb2aa6c1ab46b50299c15d132
Gang Yi <khorgangyi@gmail.com>
2024-04-21 18:48:57 +0100

    Server check error

--- src/pages/api/isSetup/isSetupServices.ts
+++ src/pages/api/isSetup/isSetupServices.ts
@@ -11,6 +11,7 @@ export default async function isSetupServices(): Promise<isSetupResponses> {
 
 		return { status: haventPublishCount > 0 };
 	} catch (err) {
+		console.error(err);
 		throw err;
 	}
 }
