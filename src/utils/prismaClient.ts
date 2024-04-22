d1fa840576ac50a26b2cc833bea0e73f46596deb
Gang Yi <khorgangyi@gmail.com>
2024-04-22 21:35:47 +0100

    Revert "Applied dynamic prisma db url based on env"
    This reverts commit 8307ab164de2b26b5928bf547cf90430516bc71d.

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-	return new PrismaClient({
-		datasources: {
-			db: {
-				url: process.env.POSTGRES_PRISMA_URL,
-			},
-		},
-	});
+	return new PrismaClient();
 };
 
 declare global {
